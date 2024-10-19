9e9d7db0ab2501411384c3b8448ec47890e7ea61
Frescghzt <frescghzt@gmail.com>
2024-10-19 16:42:16 -0500

    feat(personajes): se añadieron todos los personajes

--- src/App.java
+++ src/App.java
@@ -94,6 +94,7 @@ public class App {
             //creamos una variable apra el while
         Boolean isSeleccion = true;
         while (isSeleccion) {
+            System.out.print("\033[2J");
             //menu de personajes
             System.out.println("");
             System.out.println("INGRESE EL CODIGO DEL PERSONAJE QUE DESEA VER LAS CARACTERISTICAS");
@@ -118,6 +119,7 @@ public class App {
                 }
             }
             System.out.print("\033[2J");
+            int codeSeleccion;
             //descripcion
             switch (codePersonajes) {
                 //descrpcion speed flash
@@ -137,7 +139,7 @@ public class App {
                 System.out.println("------------------------------------------------------------------------------------------");
                 System.out.println("1. SELECCIONAR PERSONAJE");
                 System.out.println("2. SELECCIONAR OTRO PERSONAJE"); 
-                int codeSeleccion;
+                
                 while (true) {
                     try {
                         codeSeleccion = Integer.parseInt(input.nextLine());
@@ -155,21 +157,156 @@ public class App {
                 }
                 if (codeSeleccion == 1) {                    
                     SuperHero speedFlash = new SuperHero("SPEED FLASH", 70, 95, 60);
-                } else{
                     isSeleccion = false;
-                }           
+                }                          
                 break;
                 case 2:
-                    
+                System.out.println("IRON HAMMER");
+                System.out.println("-----------");
+                System.out.println("ATRIBUTOS");
+                System.out.println("");
+                System.out.println("FUERZA: 90");
+                System.out.println("VELOCIDAD: 50");
+                System.out.println("VIDA: 80");
+                System.out.println("Descripción: Iron hammer es un héroe fuerte y resistente. Su fuerza le permite causar grandes");
+                System.out.println("daños, pero su velocidad es baja, lo que lo hace más vulnerable a ataques rápidos. Tiene una buena");
+                System.out.println("cantidad de vida que le permite aguantar más tiempo en batalla.");
+                System.out.println("-----------------------------------------------------------------------------------------");
+                System.out.println("INGRESE EL CODIGO DE LA ACCION QUE DESEA REALIZAR");
+                System.out.println("------------------------------------------------------------------------------------------");
+                System.out.println("1. SELECCIONAR PERSONAJE");
+                System.out.println("2. SELECCIONAR OTRO PERSONAJE"); 
+                
+                while (true) {
+                    try {
+                        codeSeleccion = Integer.parseInt(input.nextLine());
+                        if (codeSeleccion < 1 || codeSeleccion >2) {
+                            System.out.println("");
+                            System.out.println("DEBE INGRESAR UN CODIGO VALIDO");
+                        }else{
+                            break;
+                        }
+    
+                    } catch (NumberFormatException error) {
+                        System.out.println("");
+                        System.out.println("DEBE INGRESAR UN CODIGO VALIDO");                        
+                    }                   
+                }
+                if (codeSeleccion == 1) {                    
+                    SuperHero ironHammer = new SuperHero("IRON HAMMER", 70, 95, 60);
+                    isSeleccion = false;
+                }           
                     break;
                 case 3:
-                    
+                System.out.println("SILENT SHADOW");
+                System.out.println("-----------");
+                System.out.println("ATRIBUTOS");
+                System.out.println("");
+                System.out.println("FUERZA: 65");
+                System.out.println("VELOCIDAD: 85");
+                System.out.println("VIDA: 70");
+                System.out.println("Descripción: Silent shadow es un maestro del sigilo y la agilidad. Tiene una velocidad superior que");
+                System.out.println("le permite moverse rápidamente y atacar desde las sombras. Su fuerza es moderada, lo que le");
+                System.out.println("permite hacer un daño decente, y tiene una cantidad razonable de vida para sobrevivir en combate.");
+                System.out.println("-----------------------------------------------------------------------------------------");
+                System.out.println("INGRESE EL CODIGO DE LA ACCION QUE DESEA REALIZAR");
+                System.out.println("------------------------------------------------------------------------------------------");
+                System.out.println("1. SELECCIONAR PERSONAJE");
+                System.out.println("2. SELECCIONAR OTRO PERSONAJE"); 
+                
+                while (true) {
+                    try {
+                        codeSeleccion = Integer.parseInt(input.nextLine());
+                        if (codeSeleccion < 1 || codeSeleccion >2) {
+                            System.out.println("");
+                            System.out.println("DEBE INGRESAR UN CODIGO VALIDO");
+                        }else{
+                            break;
+                        }
+    
+                    } catch (NumberFormatException error) {
+                        System.out.println("");
+                        System.out.println("DEBE INGRESAR UN CODIGO VALIDO");                        
+                    }                   
+                }
+                if (codeSeleccion == 1) {                    
+                    SuperHero silentShadow = new SuperHero("SILENT SHADOW", 65, 85, 70);
+                    isSeleccion = false;
+                }                    
                     break;
                 case 4:
-                    
+                System.out.println("STONE COLOSSUS");
+                System.out.println("-----------");
+                System.out.println("ATRIBUTOS");
+                System.out.println("");
+                System.out.println("FUERZA: 95");
+                System.out.println("VELOCIDAD: 40");
+                System.out.println("VIDA: 100");
+                System.out.println("Descripción: Stone colossus es un gigante imponente con una fuerza sobrehumana. Puede resistir");
+                System.out.println("mucho daño gracias a su alta vida, pero su velocidad es muy baja. Esto significa que es un tanque en");
+                System.out.println("el campo de batalla, ideal para absorber golpes y proteger a sus aliados.");
+                System.out.println("-----------------------------------------------------------------------------------------");
+                System.out.println("INGRESE EL CODIGO DE LA ACCION QUE DESEA REALIZAR");
+                System.out.println("------------------------------------------------------------------------------------------");
+                System.out.println("1. SELECCIONAR PERSONAJE");
+                System.out.println("2. SELECCIONAR OTRO PERSONAJE"); 
+                
+                while (true) {
+                    try {
+                        codeSeleccion = Integer.parseInt(input.nextLine());
+                        if (codeSeleccion < 1 || codeSeleccion >2) {
+                            System.out.println("");
+                            System.out.println("DEBE INGRESAR UN CODIGO VALIDO");
+                        }else{
+                            break;
+                        }
+    
+                    } catch (NumberFormatException error) {
+                        System.out.println("");
+                        System.out.println("DEBE INGRESAR UN CODIGO VALIDO");                        
+                    }                   
+                }
+                if (codeSeleccion == 1) {                    
+                    SuperHero stoneColossus= new SuperHero("STONE COLOSSUS", 95, 40, 100);
+                    isSeleccion = false;
+                }   
                     break;
+
                 case 5:
-                    
+                System.out.println("RADIANT FLASH");
+                System.out.println("-----------");
+                System.out.println("ATRIBUTOS");
+                System.out.println("");
+                System.out.println("FUERZA: 80");
+                System.out.println("VELOCIDAD: 75");
+                System.out.println("VIDA: 65");
+                System.out.println("Descripción: Radiant flash combina un equilibrio entre fuerza y velocidad. Es capaz de infligir un");
+                System.out.println("daño considerable mientras se mueve ágilmente por el campo de batalla. Su vida es moderada, lo");
+                System.out.println("que le permite ser efectivo tanto en ataque como en defensa.");
+                System.out.println("-----------------------------------------------------------------------------------------");
+                System.out.println("INGRESE EL CODIGO DE LA ACCION QUE DESEA REALIZAR");
+                System.out.println("------------------------------------------------------------------------------------------");
+                System.out.println("1. SELECCIONAR PERSONAJE");
+                System.out.println("2. SELECCIONAR OTRO PERSONAJE"); 
+                
+                while (true) {
+                    try {
+                        codeSeleccion = Integer.parseInt(input.nextLine());
+                        if (codeSeleccion < 1 || codeSeleccion >2) {
+                            System.out.println("");
+                            System.out.println("DEBE INGRESAR UN CODIGO VALIDO");
+                        }else{
+                            break;
+                        }
+                    } catch (NumberFormatException error) {
+                        System.out.println("");
+                        System.out.println("DEBE INGRESAR UN CODIGO VALIDO");                        
+                    }                   
+                }
+                if (codeSeleccion == 1) {                    
+                    SuperHero radiantFlash= new SuperHero("RADIANT FLASH", 80, 75, 65);
+                    isSeleccion = false;
+                }  
                     break;
             }
         }
@@ -184,5 +321,6 @@ public class App {
                 
                 break;
         }
+        input.close();
     }
 }
